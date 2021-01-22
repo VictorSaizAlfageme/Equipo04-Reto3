bee05f93a651064fc1aa61045d4f802feb9868d7
Naia <naia.idgalo@gmail.com>
2021-01-22 10:52:35 +0100

    fn-interfazObras-10:50

--- resources/views/obrasSolicitante.blade.php
+++ resources/views/obrasSolicitante.blade.php
@@ -4,46 +4,88 @@
         <div class="row">
             <div class="col-md-12">
                 <!-- Tabla obras -->
-                <table class="table table-condensed"  id="myTable">
+                <table class="table">
                     <thead>
-                    <tr>
-                        <th>Nº OBRA</th>
-                        <th>TIPO DE OBRA</th>
-                        <th>FECHA INICIO</th>
-                        <th>FECHA FINAL</th>
-                        <th>PROGRESO</th>
-                    </tr>
+                        <tr>
+                            <th class="text-center">Nº</th>
+                            <th>COD. OBRA</th>
+                            <th>TIPO DE OBRA</th>
+                            <th>ESTADO</th>
+                            <th>FECHA INICIO</th>
+                            <th>FECHA FIN</th>
+                            <th></th>
+                        </tr>
                     </thead>
-                    <tbody class="panel">
-                        <tr class="accordion-button" type="button" data-toggle="collapse" data-target="#demo1" data-parent="#myTable" aria-expanded="true" aria-controls="collapseOne">
-                            <td>1</td>
-                            <td>05 May 2013</td>
-                            <td>Credit Account</td>
-                            <td>$150.00</td>
-                            <td>$150.00</td>
+                    <tbody class="table-body">
+                        <tr class="cell-1" data-toggle="collapse" data-target="#demo">
+                            <td class="text-center">1</td>
+                            <td>09485</td>
+                            <td>Reparación</td>
+                            <td><span class="badge badge-dark">Valorando</span></td>
+                            <td>20/01/2021</td>
+                            <td>Sin definir</td>
+                            <td class="table-elipse" data-toggle="collapse" data-target="#demo"><i class="fas fa-angle-down"></i></td>
                         </tr>
-                        <tr id="demo1" class="collapse">
-                            <td colspan="6" class="hiddenRow"><div>Demo1</div> </td>
+                        <tr id="demo" class="collapse cell-1 row-child">
+                            <td colspan="2"><b>Último comentario:</b></td>
+                            <td colspan="3">Estamos valorando tu solicitud</td>
+                            <td colspan="2"><button type="button" class="btn btn-primary">Contactar</button></td>
                         </tr>
-                        <tr data-toggle="collapse" data-target="#demo2" data-parent="#myTable">
-                            <td>2</td>
-                            <td>05 May 2013</td>
-                            <td>Credit Account</td>
-                            <td>$11.00</td>
-                            <td>$161.00</td>
+                        <tr class="cell-1" data-toggle="collapse" data-target="#demo-2">
+                            <td class="text-center">1</td>
+                            <td>09485</td>
+                            <td>Reparación</td>
+                            <td><span class="badge badge-secondary">Aceptado</span></td>
+                            <td>20/01/2021</td>
+                            <td>Sin definir</td>
+                            <td class="table-elipse" data-toggle="collapse" data-target="#demo"><i class="fas fa-angle-down"></i></td>
                         </tr>
-                        <tr id="demo2" class="collapse">
-                            <td colspan="6" class="hiddenRow"><div>Demo2</div></td>
+                        <tr id="demo-2" class="collapse cell-1 row-child">
+                            <td colspan="2"><b>Último comentario:</b></td>
+                            <td colspan="3">Hemos aceptado tu solicitud. Pronto tendrás noticias</td>
+                            <td colspan="2"><button type="button" class="btn btn-primary">Contactar</button></td>
                         </tr>
-                        <tr data-toggle="collapse" data-target="#demo3" data-parent="#myTable">
-                            <td>3</td>
-                            <td>05 May 2013</td>
-                            <td>Credit Account</td>
-                            <td>$500.00</td>
-                            <td>$661.00</td>
+                        <tr class="cell-1" data-toggle="collapse" data-target="#demo-3">
+                            <td class="text-center">1</td>
+                            <td>09485</td>
+                            <td>Reparación</td>
+                            <td><span class="badge badge-info">Denegado</span></td>
+                            <td>20/01/2021</td>
+                            <td>Sin definir</td>
+                            <td class="table-elipse" data-toggle="collapse" data-target="#demo"><i class="fas fa-angle-down"></i></td>
                         </tr>
-                        <tr id="demo3" class="collapse">
-                            <td colspan="6" class="hiddenRow"><div>Demo3</div></td>
+                        <tr id="demo-3" class="collapse cell-1 row-child">
+                            <td colspan="2"><b>Último comentario:</b></td>
+                            <td colspan="3">Tu solicitud ha sido denegada. Para más información contacta con nosotros</td>
+                            <td colspan="2"><button type="button" class="btn btn-primary">Contactar</button></td>
+                        </tr>
+                        <tr class="cell-1" data-toggle="collapse" data-target="#demo-4">
+                            <td class="text-center">1</td>
+                            <td>09485</td>
+                            <td>Reparación</td>
+                            <td><span class="badge badge-light">En proceso</span></td>
+                            <td>20/01/2021</td>
+                            <td>Sin definir</td>
+                            <td class="table-elipse" data-toggle="collapse" data-target="#demo"><i class="fas fa-angle-down"></i></td>
+                        </tr>
+                        <tr id="demo-4" class="collapse cell-1 row-child">
+                            <td colspan="2"><b>Último comentario:</b></td>
+                            <td colspan="3">¡¡Estamos con la obra!!</td>
+                            <td colspan="2"><button type="button" class="btn btn-primary">Contactar</button></td>
+                        </tr>
+                        <tr class="cell-1" data-toggle="collapse" data-target="#demo-4">
+                            <td class="text-center">1</td>
+                            <td>09485</td>
+                            <td>Reparación</td>
+                            <td><span class="badge badge-success">Finalizado</span></td>
+                            <td>20/01/2021</td>
+                            <td>Sin definir</td>
+                            <td class="table-elipse" data-toggle="collapse" data-target="#demo"><i class="fas fa-angle-down"></i></td>
+                        </tr>
+                        <tr id="demo-4" class="collapse cell-1 row-child">
+                            <td colspan="2"><b>Último comentario:</b></td>
+                            <td colspan="3">Hemos finalizado tu obra. ¡¡Disfrutala!!</td>
+                            <td colspan="2"><button type="button" class="btn btn-primary">Contactar</button></td>
                         </tr>
                     </tbody>
                 </table>
