c47c4ab0fce7c471d5df21b371e783feb58e4e9c
Naia <naia.idgalo@gmail.com>
2021-01-26 14:02:43 +0100

    fn-cambios-14:02

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -291,15 +291,7 @@
         @yield('content')
 
         <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-            <div class="container my-auto">
-                <div class="footer-class copyright text-center my-auto">
-                    <img class="footer-img vitoria1" src="img/footer01.jpg">
-                    <span>Copyright &copy; NUVE 2020</span>
-                    <img class="footer-img vitoria2" src="img/footer02.png">
-                </div>
-            </div>
-        </footer>
+        @include('footer')
         <!-- End of Footer -->
 
     </div>
