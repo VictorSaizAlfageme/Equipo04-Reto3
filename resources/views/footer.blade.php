d0f9dedff84f2ee988ec6d494f6db638b3261a1c
Naia <naia.idgalo@gmail.com>
2021-02-01 12:27:37 +0100

    fn-webcomponent-12:27

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,14 +1,5 @@
-<footer>
-    <template id="footer-fijado">
-        <footer class="bg-white">
-            <div class="footer-class copyright text-center my-auto">
-                <img class="footer-img vitoria1" src="img/footer01.jpg">
-                <span>Copyright &copy; NUVE 2020</span>
-                <img class="footer-img vitoria2" src="img/footer02.png">
-            </div>
-        </footer>
-    </template>
-</footer>
+<div id="footer-fijado">
+</div>
 
 
 
