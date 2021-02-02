8e9d6b207b34568bcfb1cc13d28a0403584398ae
Nuve <nuve.info@gmail.com>
2021-02-02 12:19:59 +0100

    fn-webComponent-12:19

--- public/js/webComponent.js
+++ public/js/webComponent.js
@@ -2,17 +2,13 @@
 class FooterFijado extends HTMLElement{
     constructor(){
         super();
-        const template = document.createElement('template');
-        template.innerHTML =
-            '<style> footer{\n' +
-            '    position: inherit;\n' +
-            '    bottom: 0;\n' +
-            '    text-align: center;\n' +
-            '    background-color: white;' +
-            '    padding-top: 25px;' +
-            '    padding-bottom: 25px;   }</style>';
-        const shadowRoot = this.attachShadow({mode:'open'});
-        shadowRoot.appendChild(template.content);
+
+    }
+    connectedCallback(){
+        this.innerHTML = '<img class="footer-img vitoria1" src="img/footer01.jpg">  ' +
+            '<span>Copyright &copy; NUVE 2020</span> ' +
+            '\n' +
+            ' <img class="footer-img vitoria2" src="img/footer02.png">'
     }
 }
 
