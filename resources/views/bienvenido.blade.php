363775a548328e682edb6bb6be24e0f2f983b870
Victor Saiz Alfageme <56159067+VictorSaizAlfageme@users.noreply.github.com>
2021-02-03 22:47:55 +0100

    Update bienvenido.blade.php

--- resources/views/bienvenido.blade.php
+++ resources/views/bienvenido.blade.php
@@ -25,7 +25,7 @@
         <p class="font-weight-lighter">Esta web quiere ser también testigo del crecimiento de NUVE, desde sus orígenes hasta su incursión en aquellos sectores demandados por la sociedad.</p>
         <br>
         <p class="font-weight-lighter">Pero el objetivo principal de este canal de comunicación es plasmar los <b>valores</b> que nos respaldan: el compromiso social, la máxima calidad y la voluntad de servicio, así como nuestro interés por todas aquellas ventajas que nos ofrece la tecnología. Todo ello tiene una presencia destacada en esta página web y en nuestras propias decisiones.</p>
-        <figcaption class="blockquote-footer font-weight-lighter">Victor Saiz, Eric Martínez y Naia Ibañez de Garayo</figcaption>
+        <figcaption class="blockquote-footer font-weight-lighter">Víctor Sáiz, Eric Martínez y Naia Ibáñez de Garayo</figcaption>
         <br>
     </div>
     <div style="max-width: 300px" class="col-0 col-md-4 col-xl-3 offset-2 offset-md-0 offset-xl-1 align-self-center">
